d88de97886e251a3c56a3d01ba23b517111f13b7
Marty Wallace <martyaced@gmail.com>
2014-06-23 16:52:55 +1000

    Adding form controls to Tempest.js.

--- static/js/vendor/tempest.js
+++ static/js/vendor/tempest.js
@@ -124,4 +124,80 @@
 
 	})();
 
+
+
+	// Manages forms - pushes data through via <code>Tempest.api()</code>. Cleans up data before send,
+	// and calls a relevant callback handler when a response is received.
+	$("form[action][method] button").on("click", function(event)
+	{
+		var form = $(this).parents("form");
+
+		if(form.data("default") === "true")
+		{
+			// Perform normal form submission - ignore Tempest rules below.
+			return;
+		}
+
+		event.preventDefault();
+
+		if($(this).is("[type=submit]"))
+		{
+			var data = { };
+
+			// Iterate over input controls and collect data.
+			form.find("input, textarea, select").each(function()
+			{
+				var field = $(this);
+				var name = field.attr("name") || field.data("name") || null;
+
+				if(name === null || field.is("[type=button]") || field.is("[type=file]"))
+				{
+					// Can't work with fields that have no name, are intended for file uploads, or
+					// that are buttons.
+					return;
+				}
+
+				// General input via <code>.val()</code>.
+				if(field.is("[type=text]") ||
+				   field.is("[type=email]") ||
+				   field.is("[type=password]") ||
+				   field.is("[type=hidden]") ||
+				   field.is("select") ||
+				   field.is("textarea"))
+				{
+					data[name] = field.val();
+				}
+
+				// Checkboxes - more suitable/modern behaviour than default.
+				if(field.is("[type=checkbox]"))
+				{
+					data[name] = field.is(":checked") ? 1 : 0;
+				}
+
+				// Ratio buttons.
+				if(field.is("[type=radio]"))
+				{
+					data[name] = form.find("input[type=radio][name=" + name + "]:checked").val() || "";
+				}
+
+			});
+
+
+			console.log(data);
+
+			// Post via Tempest.
+			Tempest.api(form.attr("method"), form.attr("action"), data, function(response)
+			{
+				if(form.is("[data-callback]"))
+				{
+					// TODO: Pass response to callback handler.
+					console.log(response);
+				}
+
+			});
+		}
+
+
+	});
+
 })();
