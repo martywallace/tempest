11a36ed16ca259e89a093a516f1f4f87bbca2e77
Marty Wallace <martyaced@gmail.com>
2015-03-20 00:27:07 +1100

    Moving filter code to utility classes.

--- server/tempest/Tempest/Twig/Filters.php
+++ server/tempest/Tempest/Twig/Filters.php
@@ -10,7 +10,8 @@ class Filters extends Extensions
 	protected function defineExtensions()
 	{
 		return array(
-			'pluck' => array($this, 'pluck')
+			'pluck' => 'Tempest\Utils\ArrayUtil::pluck',
+			'hyphenate' => 'Tempest\Utils\StringUtil::hyphenate'
 		);
 	}
 
@@ -18,28 +19,6 @@ class Filters extends Extensions
 	protected function addExtension(Twig_Environment $environment, $handle, $callable)
 	{
 		$environment->addFilter(new Twig_SimpleFilter($handle, $callable));
-	}
-
-
-	/**
-	 * Pluck the value of a specified property from each item in an input array and return the
-	 * result array.
-	 *
-	 * @param array $input The input array.
-	 * @param string $property The property to pluck from each item in the input array.
-	 *
-	 * @return array
-	 */
-	public function pluck($input, $property)
-	{
-		$output = array();
-		foreach ($input as $object)
-		{
-			if (is_array($object) && array_key_exists($property, $object)) $output[] = $object[$property];
-			if (is_object($object) && property_exists($object, $property)) $output[] = $object->{$property};
-		}
-
-		return $output;
 	}
 
 }
--- server/tempest/Tempest/Utils/StringUtil.php
+++ server/tempest/Tempest/Utils/StringUtil.php
@@ -2,6 +2,7 @@
 
 /**
  * Utilities for working with Strings.
+ *
  * @author Marty Wallace.
  */
 class StringUtil
@@ -9,16 +10,18 @@ class StringUtil
 
 	/**
 	 * Replace non-word characters with hyphens and return the lowercased result.
+	 *
 	 * @param string $value The input string.
 	 */
 	public static function hyphenate($value)
 	{
-		return strtolower(preg_replace('/[^\w]+/', '-', $value));
+		return trim(strtolower(preg_replace('/[^\w]+/', '-', $value)), '-');
 	}
 
 
 	/**
 	 * Returns the first item from the result of <code>explode()</code> on the input value.
+	 *
 	 * @param string $value The input value.
 	 * @param string $separator The separator to use for <code>explode()</code>.
 	 */
