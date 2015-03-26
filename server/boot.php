c4a27f993910a089f43d573054b19a39e1267c64
Marty Wallace <martyaced@gmail.com>
2015-03-26 16:13:50 +1100

    Fixed bug where router ignored requests with a single trailing ?.

--- server/boot.php
+++ server/boot.php
@@ -14,5 +14,5 @@ define('POST', 'post');
 define('NAMED', 'named');
 
 define('HOST', strtolower($_SERVER['SERVER_NAME']));
-define('REQUEST_CLEAN', preg_replace('/(\?|#)(.+)/', '', $_SERVER["REQUEST_URI"]));
+define('REQUEST_CLEAN', preg_replace('/(\?|#)(.*)/', '', $_SERVER["REQUEST_URI"]));
 define('REQUEST_URI', Path::create(REQUEST_CLEAN, Path::DELIMITER_LEFT));
