5662ed2d0a714be1a985a6bebd1d86276cf35f0b
Marty <martyaced@gmail.com>
2015-05-14 21:57:40 +1000

    Gitignore correction.

---
+++ vendor/martywallace/tempest/README.md
@@ -0,0 +1,3 @@
+# Temepest v2.
+
+This is the Tempest core used by the outer shell that is the Tempest framework as a whole.
---
+++ vendor/martywallace/tempest/src/Tempest/Tempest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tempest;
+
+
+/**
+ * Tempest's core.
+ *
+ * @author Marty Wallace.
+ */
+class Tempest
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function start()
+    {
+        echo 'It works!';
+    }
+
+}
