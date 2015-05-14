4a8d076f848d0ab96d2cc71bb9d054307260683f
Marty <martyaced@gmail.com>
2015-05-14 20:41:54 +1000

    Cleanup for v2.

--- public/index.php
+++ public/index.php
@@ -1,25 +1,3 @@
 <?php
 
-require_once __DIR__ . '/../server/boot.php';
-
-/**
- * Returns the active application instance.
- *
- * @return App
- */
-function tempest()
-{
-	if(App::getInstance() === null)
-	{
-		$app = new App();
-		$app->start();
-
-		return $app;
-	}
-
-	return App::getInstance();
-}
-
-
-// Initialize the application.
-tempest();
+echo 'It works!';
