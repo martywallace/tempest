2e98ea3d25575905299f90e6ed54f41ca5b86136
Marty <martyaced@gmail.com>
2015-05-14 21:56:00 +1000

    Set up package.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Tempest.
+# Tempest v2.
 
 Yet another PHP framework by [Marty Wallace](http://martywallace.com).
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo 'It works!';
+define('ROOT', __DIR__ . '/../');
+
+require(ROOT . '/vendor/autoload.php');
+
+$app = new \Tempest\Tempest();
+$app->start();
