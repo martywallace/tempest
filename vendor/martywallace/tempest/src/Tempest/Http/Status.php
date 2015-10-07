5697607ad2906c5d7ab9ede2d6fdc24335088721
Marty <martyaced@gmail.com>
2015-10-07 11:24:16 +1100

    Corrected HTTP status values for redirection.

--- vendor/martywallace/tempest/src/Tempest/Http/Status.php
+++ vendor/martywallace/tempest/src/Tempest/Http/Status.php
@@ -9,8 +9,8 @@
 class Status {
 
 	const OK = 200;
-	const FOUND = 301;
-	const MOVED_PERMANENTLY = 302;
+	const MOVED_PERMANENTLY = 301;
+	const FOUND = 302;
 	const NOT_FOUND = 404;
 	const METHOD_NOT_ALLOWED = 405;
 	const INTERNAL_SERVER_ERROR = 500;
