35b07271761c388211ee86c27e68eeb9110386fd
Marty Wallace <marty@simplywallst.com>
2018-01-18 09:17:01 +1100

    Slack logs.

--- src/Tempest/Services/LogService.php
+++ src/Tempest/Services/LogService.php
@@ -3,6 +3,7 @@
 use Tempest\App;
 use Monolog\Logger;
 use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\SlackHandler;
 
 /**
  * Handles application logging.
@@ -16,6 +17,16 @@ class LogService extends Logger implements Service {
 
 		if (!empty(App::get()->storage)) {
 			$handler = new RotatingFileHandler(App::get()->storage . '/logs/app.log');
+			$this->pushHandler($handler);
+		}
+
+		if (!empty(App::get()->config('slack'))) {
+			$handler = new SlackHandler(
+				App::get()->config('slack.token'),
+				App::get()->config('slack.channel'),
+				App::get()->config('slack.name', 'Log')
+			);
+
 			$this->pushHandler($handler);
 		}
 	}
